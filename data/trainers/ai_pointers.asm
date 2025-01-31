b48c0147bed817b6654720796ba304b099fbfd56
M4 <dave.was.r@gmail.com>
2025-01-31 05:01:12 -0500

    Changed host trainer AI

--- data/trainers/ai_pointers.asm
+++ data/trainers/ai_pointers.asm
@@ -57,9 +57,9 @@ TrainerAIPointers:
 	dbw 2, BlackbeltAI ; Jack
 	dbw 2, GenericAI ; Jessie & James
 	dbw 3, SabrinaAI
-	dbw 3, SabrinaAI ; Kris
+	dbw 3, LanceAI ; Kris
 	dbw 3, LanceAI ; CCC
 	dbw 3, Rival3AI ; KAY
 	dbw 3, LanceAI ; BBBBBa
-	dbw 3, SabrinaAI ; ROCKETA
+	dbw 3, LanceAI ; ROCKETA
 	assert_table_length NUM_TRAINERS
